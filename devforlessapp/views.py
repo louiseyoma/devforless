8652c25ee86332c1fcb09901fff706e248e1231d
louiseyoma <louiseyoma@yahoo.com>
2018-01-19 23:44:00 +0100

    second commit

---
+++ devforlessapp/views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render, HttpResponse
+from .forms import FormForLogin, CustomUserCreationForm, JobPostForm
+from django.http import HttpResponseRedirect
+from django.utils import timezone
+
+
+
+def home(request):
+    return render(request, 'devforlessapp/home.html')
+
+def careers(request):
+    return render(request, 'devforlessapp/careers.html')
+
+def signup(request):
+	if request.method == 'POST':
+		f = CustomUserCreationForm(request.POST)
+		if f.is_valid():
+			f.save()
+			#messages.success(request, 'Account created successfully')
+			return HttpResponseRedirect('signup_suc')
+	else:
+		f = CustomUserCreationForm()
+	return render(request, 'devforlessapp/signup.html', {'form': f})
+
+
+
+def suc(request):
+	return render(request, 'devforlessapp/success_page.html')
+
+
+
+def signup_suc(request):
+	return render(request, 'devforlessapp/signup_success.html')
+
+
+
+
+
+
+
+
+def post_job(request):
+
+	if request.method == "POST":
+		form = JobPostForm(request.POST)
+		if form.is_valid():
+			jobpost = form.save(commit=False)
+			jobpost.employer = request.user
+			jobpost.time_of_post_creation = timezone.now()
+			jobpost.save()
+			return HttpResponseRedirect('suc')
+	else:
+		form = JobPostForm()
+	return render(request, 'devforlessapp/post_job.html', {'form': form} )
+
+
+def login(request):
+
+	if request.method == 'POST':
+		form = FormForLogin(request.POST)
+		if form.is_valid():
+			useremail = form.cleaned_data.get("email")
+			password = form.cleaned_data.get("password")
+
+			return HttpResponseRedirect('home')
+	else:
+		form = FormForLogin()
+	return render(request, 'devforlessapp/login.html', {'form': form})
+
+
+'''
+def register(request):
+	if request.method == 'POST':
+		f = CustomUserCreationForm(request.POST)
+		if f.is_valid():
+			f.save()
+			#messages.success(request, 'Account created successfully')
+			return HttpResponseRedirect('register')
+
+	else:
+		f = CustomUserCreationForm()
+	return render(request, 'devforlessapp/register.html', {'form': f})
+'''
